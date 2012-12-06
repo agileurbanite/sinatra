e635a17115a703f53be6810494f69f61bccf2feb
Patrick Woo <agileurbanite@gmail.com>
2012-12-06 12:04:16 -0800

    First duet with sinatra

--- basics.rb
+++ basics.rb
@@ -12,3 +12,23 @@ end
 get '/hello/:name' do
   "Hello there, #{params[:name]}."
 end
+
+get '/form' do
+  erb :form
+end
+
+post '/form' do
+  "You said '#{params[:message]}'"
+end
+
+get '/secret' do
+  erb :secret
+end
+
+post '/secret' do
+  params[:secret].reverse
+end
+
+get '/decrypt/:secret' do
+  params[:secret].reverse
+end
